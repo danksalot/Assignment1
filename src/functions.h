23d8c17b6a94f2fd3aa82ab40d414676ae2abaf3
danksalot <danksalot@users.noreply.github.com>
2016-10-28 11:05:09 -0500

    Create functions.h

---
+++ src/functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+int calculateMaxHours();
+int getInputFromUser(std::string, int);
+int calculateSeconds(int, int, int);
+std::string formatOuput(int, int, int);
+
+#endif
